676edf1a97a4ab692fa796be3065ccfa4bd262aa
minimec <minimec@gmail.com>
2013-04-11 14:05:31 +0200

    @override

--- donne/s04_e17/src/expression/SubstactionExpression.java
+++ donne/s04_e17/src/expression/SubstactionExpression.java
@@ -5,18 +5,22 @@ import composite.ArithmeticExpression;
 
 public class SubstactionExpression extends ArithmeticExpression {
 
+        @Override
 	public int depth() {
 		throw new UnsupportedOperationException();
 	}
 
+        @Override
 	public int eval() {
 		throw new UnsupportedOperationException();
 	}
 
+        @Override
 	public String prefixPrint() {
 		throw new UnsupportedOperationException();
 	}
 
+        @Override
 	public String prettyPrint() {
 		throw new UnsupportedOperationException();
 	}
