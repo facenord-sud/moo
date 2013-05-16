76fd8a06ca191bef0276a024f789f9e833a99348
leo <facenord.sud@gmail.com>
2013-05-16 10:47:00 +0200

    le début de la factory

---
+++ donne/simj/src/main/factory/Factory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+/**
+ *
+ * @author Numa de Montmollin <facenord.sud@gmail.com>
+ */
+public class Factory {
+    
+}
